0bb39c8bd8bca6e50e65a7a3dc55cf57fd8aed8a
Maria <maria.garciaovies@opendeusto.es>
2022-11-25 09:36:19 +0100

    Hola

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@
 #define WAIT_TIME_MS 500 
 DigitalOut led1(LED1);
 Grove_LCD_RGB_Backlight rgbLCD(PB_9,PB_8);
-
+int c=0;
+char cadena[16];
 int main()
 {
     printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
@@ -17,7 +18,12 @@ int main()
     while (true)
     {
         rgbLCD.setRGB(0xff, 0xff, 0xff);                 //set the color 
-        rgbLCD.print("Hello World!");
+        rgbLCD.locate(0,0);
+        rgbLCD.print("Hola buenos dias");
+        c++;
+        sprintf(cadena,"%d",c);
+        rgbLCD.locate(0,1);
+        rgbLCD.print(cadena);
         led1 = !led1;
         thread_sleep_for(WAIT_TIME_MS);
     }
